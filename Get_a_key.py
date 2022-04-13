b74972ae73ab62ddc29ae26c79a8fb55bbdd4247
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-04-13 19:34:34 +0100

    Add files via upload

---
+++ Get_a_key.py
@@ -0,0 +1,7 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"],
+                   "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+
+print(zodiac_elements["earth"])
+print("")
+print(zodiac_elements["fire"])
